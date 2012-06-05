37e15918be7b2c032463d87ea42043582194c8df
Aleksey Drobnych <adrobnych@gmail.com>
2012-06-05 19:46:07 +0300

    service ballanced but there are some problems

--- src/com/ganttzilla/apps/UpdateWidgetService.java
+++ src/com/ganttzilla/apps/UpdateWidgetService.java
@@ -1,14 +1,27 @@
 package com.ganttzilla.apps;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
+import java.util.TimerTask;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -16,70 +29,86 @@ import android.widget.RemoteViews;
 public class UpdateWidgetService extends Service {
 
 	private static final String LOG = "de.vogella.android.widget.example";
-
+	
+	Context context;
+	Intent intent;
+	
+	Timer timer	= new Timer();
+	MyTimerTask timerTask = new MyTimerTask();
+	
 	@Override
-	public void onStart(Intent intent, int startId) {
-		Log.i(LOG, "Called");
-		// Create some random data
-
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-				//.getApplicationContext());
-
-		int[] allWidgetIds = intent
-				.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-
-		ComponentName thisWidget = new ComponentName(getApplicationContext(),
-				WebSitebrokenWidgetProvider.class);
-		int[] allWidgetIds2 = appWidgetManager.getAppWidgetIds(thisWidget);
-		Log.w(LOG, "From Intent" + String.valueOf(allWidgetIds.length));
-		Log.w(LOG, "Direct" + String.valueOf(allWidgetIds2.length));
-		
-		for(int i=0; i<5; i++){
-			try {
-				long numMillisecondsToSleep = 3000; // 3 seconds
-				Thread.sleep(numMillisecondsToSleep);
-			} catch (InterruptedException e) {
-			}
-			for (int widgetId : allWidgetIds) {
-				// Create some random data
-				int number = (new Random().nextInt(100));
-
-
-				RemoteViews remoteViews = new RemoteViews(this
-						.getApplicationContext().getPackageName(),
-						R.layout.main);
-
-
-				Log.w("WidgetExample number", (new Integer(number)).toString() );
-				// Set the text
-				remoteViews.setTextViewText(R.id.textView1,
-						"Random: " + (new Integer(number)).toString() );
-				appWidgetManager.updateAppWidget(widgetId, remoteViews);
-
-				// Register an onClickListener
-				Intent clickIntent = new Intent(this.getApplicationContext(),
-						WebSitebrokenWidgetProvider.class);
-
-				clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-				clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
-						allWidgetIds);
-
-				PendingIntent pendingIntent = PendingIntent.getBroadcast(
-						getApplicationContext(), 0, clickIntent, 
-						PendingIntent.FLAG_UPDATE_CURRENT);
-				remoteViews.setOnClickPendingIntent(R.id.imageButton1, pendingIntent);
-
-				appWidgetManager.updateAppWidget(widgetId, remoteViews);
-			}
-		}
-		stopSelf();
-
-		super.onStart(intent, startId);
+	public void onCreate() {
+		context = this;
+		 
+	}
+	
+	@Override
+	public void onStart(Intent _intent, int startId) {
+		intent = _intent;
+		timer.schedule(timerTask, 1000, 2000);
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
+	
+	private class MyTimerTask extends TimerTask {
+		  @Override
+		  public void run() {
+			  		  
+			  Log.i(LOG, "Called");
+				// Create some random data
+
+				AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+						//.getApplicationContext());
+
+				int[] allWidgetIds = intent
+						.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+
+				ComponentName thisWidget = new ComponentName(getApplicationContext(),
+						WebSitebrokenWidgetProvider.class);
+				int[] allWidgetIds2 = appWidgetManager.getAppWidgetIds(thisWidget);
+				Log.w(LOG, "From Intent" + String.valueOf(allWidgetIds.length));
+				Log.w(LOG, "Direct" + String.valueOf(allWidgetIds2.length));
+				
+					for (int widgetId : allWidgetIds) {
+						// Create some random data
+						int number = (new Random().nextInt(100));
+
+
+						RemoteViews remoteViews = new RemoteViews(context
+								.getApplicationContext().getPackageName(),
+								R.layout.main);
+
+
+						Log.w("WidgetExample number", (new Integer(number)).toString() );
+						// Set the text
+						remoteViews.setTextViewText(R.id.textView1,
+								"Random: " + (new Integer(number)).toString() );
+						appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
+						// Register an onClickListener
+//						Intent clickIntent = new Intent(context.getApplicationContext(),
+//								WebSitebrokenWidgetProvider.class);
+//
+//						clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+//						clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
+//								allWidgetIds);
+//
+//						PendingIntent pendingIntent = PendingIntent.getBroadcast(
+//								getApplicationContext(), 0, clickIntent, 
+//								PendingIntent.FLAG_UPDATE_CURRENT);
+//						remoteViews.setOnClickPendingIntent(R.id.imageButton1, pendingIntent);
+//
+//						appWidgetManager.updateAppWidget(widgetId, remoteViews);
+					}
+				}
+				//stopSelf();
+
+				//super.onStart(intent, startId);
+
+		  }
+
 
 }
--- src/com/ganttzilla/apps/WebSitebrokenWidgetProvider.java
+++ src/com/ganttzilla/apps/WebSitebrokenWidgetProvider.java
@@ -2,6 +2,8 @@ package com.ganttzilla.apps;
 
 import java.util.Random;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
@@ -17,23 +19,24 @@ public class WebSitebrokenWidgetProvider extends AppWidgetProvider {
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 
-		Log.w(LOG, "onUpdate method called!!!!!!!");
+		Log.w(LOG, "onUpdate method called!!!!!!!"); 
 		
 		onUpdateHelper(context, appWidgetManager, appWidgetIds);
 		
-		// Get all ids
-		ComponentName thisWidget = new ComponentName(context,
-				WebSitebrokenWidgetProvider.class);
-		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+		if(!isMyServiceRunning(context)){
+			// Get all ids
+			ComponentName thisWidget = new ComponentName(context,
+					WebSitebrokenWidgetProvider.class);
+			int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 
-		// Build the intent to call the service
-		Intent intent = new Intent(context.getApplicationContext(),
-				UpdateWidgetService.class);
-		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+			// Build the intent to call the service
+			Intent intent = new Intent(context.getApplicationContext(),
+					UpdateWidgetService.class);
+			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
 
-		// Update the widgets via the service
-		context.startService(intent);
-		
+			// Update the widgets via the service
+			context.startService(intent);
+		}
 		
 		
 	}
@@ -60,5 +63,14 @@ public class WebSitebrokenWidgetProvider extends AppWidgetProvider {
 		}
 	}
 	
+	private boolean isMyServiceRunning(Context context) {
+	    ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if ("com.ganttzilla.apps.UpdateWidgetService".equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
 	
 }
